d46f77ff806710a2fec38a31c493bc930c54d4eb
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2024-12-08 22:07:44 +1100

    FEAT: added basic auth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -11,3 +11,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+# TODO: parse variables via header
+@app.post("/users/login/user={user}/pass={pw}")
+def login(user: str, pw: str):
+    if user == "yunz" and pw == "<3":
+        return {"response": "true"}
+    else:
+        raise HTTPException(status_code=403, detail="Permission Denied")
